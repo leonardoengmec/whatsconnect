a7442f22c6b30b055de8fcf750c73d122d994edb
Leonardo A. Santos <leonardo.santos@connectparts.com.br>
2018-12-03 16:28:52 -0200

    Correção no comandos.py

--- modules/Connect/comandos.py
+++ modules/Connect/comandos.py
@@ -1,7 +1,8 @@
 from app.mac import mac, signals
+import os
 
 @signals.command_received.connect
 def handle(message):
     if message.command == "atualizarFoto":
-        mac.set_profile_picture("static/logo.jpg")
+        mac.set_profile_picture(os.path.abspath("static/logo.jpg"))
     
