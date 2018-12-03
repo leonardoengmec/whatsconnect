218b776e104d61ddde268bde40bb155d5d8410f4
Leonardo A. Santos <leonardo.santos@connectparts.com.br>
2018-12-03 16:12:02 -0200

    Modificações nos comandos.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,11 +1,12 @@
 # Add here the modules you want to enable here
 
 from modules.Connect import bot
+from modules.Connect import comandos
 from modules.Connect import helper
 from modules.Connect import callouts
 import pymongo
-import crontab
-import chatterbot
+#import crontab
+#import chatterbot
 #from modules import pokedex
 #from modules import poker
 #from modules.poll import poll
